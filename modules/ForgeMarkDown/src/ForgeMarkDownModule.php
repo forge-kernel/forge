7aaa57643511b6b4f2c26d96982ae27388e559e0
Jeremias Nunez <jeremias2@gmail.com>
2026-06-17 17:02:26 -0400

    Bump ForgeMarkDown module version to v0.1.1

--- modules/ForgeMarkDown/src/ForgeMarkDownModule.php
+++ modules/ForgeMarkDown/src/ForgeMarkDownModule.php
@@ -11,7 +11,7 @@ use Forge\Core\Module\Attributes\Provides;
 
 #[Module(
   name: 'ForgeMarkDown',
-  version: '0.1.0',
+  version: '0.1.1',
   description: 'A markdown processor',
   order: 70,
   author: 'Forge Team',
@@ -21,7 +21,7 @@ use Forge\Core\Module\Attributes\Provides;
 )]
 #[Service()]
 #[Compatibility(framework: '>=0.1.0', php: '>=8.3')]
-#[Provides(interface: ForgeMarkDownInterface::class, version: '0.1.0')]
+#[Provides(interface: ForgeMarkDownInterface::class, version: '0.1.1')]
 class ForgeMarkDownModule
 {
   public function register(Container $container): void
