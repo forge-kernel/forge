8d1fe94088da69666dec8f2e8d2897e6cf924e18
Jeremias Nunez <jeremias2@gmail.com>
2026-06-18 16:49:04 -0400

    Bump ForgeTesting module version to v0.4.1

--- modules/ForgeTesting/src/ForgeTestingModule.php
+++ modules/ForgeTesting/src/ForgeTestingModule.php
@@ -4,16 +4,13 @@ declare(strict_types=1);
 
 namespace App\Modules\ForgeTesting;
 
-use Forge\Core\DI\Attributes\Service;
-use Forge\Core\DI\Container;
 use Forge\Core\Module\Attributes\Compatibility;
 use Forge\Core\Module\Attributes\Module;
-use Forge\Core\Module\Attributes\Provides;
 use Forge\Core\Module\Attributes\Repository;
 
 #[Module(
     name: "ForgeTesting",
-    version: "0.4.0",
+    version: "0.4.1",
     description: "A Test Suite Module By Forge",
     order: 9999,
     isCli: true,
@@ -22,13 +19,8 @@ use Forge\Core\Module\Attributes\Repository;
     type: 'testing',
     tags: ['testing', 'unit', 'integration']
 )]
-#[Service]
 #[Compatibility(framework: ">=0.1.20", php: ">=8.3")]
 #[Repository(type: "git", url: "https://github.com/forge-kernel/kernel-module-registry")]
-#[Provides(interface: TestCase::class, version: "0.4.0")]
 final class ForgeTestingModule
 {
-    public function register(Container $container): void
-    {
-    }
 }
