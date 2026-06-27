00d9ca5bcd38031647df3d356e02ca9b9673a060
Jeremias Nunez <jeremias2@gmail.com>
2026-06-27 00:13:33 -0400

    Bump ForgeMultiTenant module version to v0.3.4

--- modules/ForgeMultiTenant/src/ForgeMultiTenantModule.php
+++ modules/ForgeMultiTenant/src/ForgeMultiTenantModule.php
@@ -19,7 +19,7 @@ use Forge\CLI\Traits\OutputHelper;
 
 #[Module(
     name: 'ForgeMultiTenant',
-    version: '0.3.3',
+    version: '0.3.4',
     description: 'A Multi Tenant Module by Forge',
     order: 2,
     author: 'Forge Team',
