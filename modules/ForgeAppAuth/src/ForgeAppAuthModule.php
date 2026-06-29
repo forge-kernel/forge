5c4200b0a6d71dda61d7772008a8fd0e8fe9d347
Jeremias Nunez <jeremias2@gmail.com>
2026-06-28 20:05:33 -0400

    Bump ForgeAppAuth module version to v0.1.2

--- modules/ForgeAppAuth/src/ForgeAppAuthModule.php
+++ modules/ForgeAppAuth/src/ForgeAppAuthModule.php
@@ -7,7 +7,7 @@ use App\Modules\ForgeAppAuth\Repositories\UserRepository;
 use App\Modules\ForgeAppAuth\Services\UserContext;
 use App\Modules\ForgeAuth\Contracts\UserContextInterface;
 use App\Modules\ForgeAuth\Contracts\UserProviderInterface;
-use Forge\Core\DI\Attributes\Service;
+use Forge\Core\Module\Attributes\Requires;
 use Forge\Core\DI\Container;
 use Forge\Core\Module\Attributes\Compatibility;
 use Forge\Core\Module\Attributes\ConfigDefaults;
@@ -19,22 +19,21 @@ use Forge\Core\Module\Attributes\Structure;
 
 #[Structure(structure: [
     'controllers' => 'src/Controllers',
-    'services'    => 'src/Services',
-    'migrations'  => 'src/Database/Migrations',
-    'views'       => 'src/UI/views',
-    'components'  => 'src/UI/views/components',
-    'commands'    => 'src/Commands',
-    'events'      => 'src/Events',
-    'tests'       => 'src/tests',
-    'models'      => 'src/Models',
-    'dto'         => 'src/Dto',
-    'seeders'     => 'src/Database/Seeders',
+    'services' => 'src/Services',
+    'migrations' => 'src/Database/Migrations',
+    'views' => 'src/UI/views',
+    'components' => 'src/UI/views/components',
+    'commands' => 'src/Commands',
+    'events' => 'src/Events',
+    'tests' => 'src/tests',
+    'models' => 'src/Models',
+    'dto' => 'src/Dto',
+    'seeders' => 'src/Database/Seeders',
     'middlewares' => 'src/Middlewares',
 ])]
-#[Service]
 #[Module(
     name: 'ForgeAppAuth',
-    version: '0.1.1',
+    version: '0.1.2',
     description: 'Distributable authentication implementation with login, register, forgot-password, and reset-password',
     order: 60,
     author: 'Forge Team',
@@ -42,6 +41,7 @@ use Forge\Core\Module\Attributes\Structure;
     type: 'generic',
     tags: ['auth', 'authentication', 'login', 'register', 'password-reset'],
 )]
+#[Requires(module: "ForgeAuth", version: ">=2.0.5")]
 #[Compatibility(framework: '>=4.15.13', php: '>=8.3')]
 #[Repository(type: 'git', url: 'https://github.com/forge-kernel/kernel-module-registry')]
 #[ConfigDefaults(defaults: [
