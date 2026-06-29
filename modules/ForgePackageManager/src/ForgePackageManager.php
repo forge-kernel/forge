cdfa019c7cc90c6065c6804ab53785a9fe91022a
Jeremias Nunez <jeremias2@gmail.com>
2026-06-28 20:23:37 -0400

    Bump ForgePackageManager module version to v3.3.19

--- modules/ForgePackageManager/src/ForgePackageManager.php
+++ modules/ForgePackageManager/src/ForgePackageManager.php
@@ -15,7 +15,7 @@ use Forge\Core\Module\Attributes\Repository;
 
 #[Module(
     name: 'ForgePackageManager',
-    version: '3.3.18',
+    version: '3.3.19',
     description: 'A Package Manager By Forge',
     order: 1,
     isCli: true,
