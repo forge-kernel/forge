5c707e5efda129cfef846862dc41aa8670538c33
Jeremias Nunez <jeremias2@gmail.com>
2026-06-30 08:16:57 -0400

    Bump ForgeMarkDown module version to v0.1.4

--- modules/ForgeMarkDown/src/ForgeMarkDownModule.php
+++ modules/ForgeMarkDown/src/ForgeMarkDownModule.php
@@ -3,29 +3,27 @@
 namespace Modules\ForgeMarkDown;
 
 use Modules\ForgeMarkDown\Contracts\ForgeMarkDownInterface;
-use Forge\Core\DI\Attributes\Service;
 use Forge\Core\DI\Container;
 use Forge\Core\Module\Attributes\Compatibility;
 use Forge\Core\Module\Attributes\Module;
 use Forge\Core\Module\Attributes\Provides;
 
 #[Module(
-  name: 'ForgeMarkDown',
-  version: '0.1.3',
-  description: 'A markdown processor',
-  order: 70,
-  author: 'Forge Team',
-  license: 'MIT',
-  type: 'html',
-  tags: ['html', 'static', 'site', 'generator', 'markdown', 'processor', 'markdown-processor']
+    name: 'ForgeMarkDown',
+    version: '0.1.4',
+    description: 'A markdown processor',
+    order: 70,
+    author: 'Forge Team',
+    license: 'MIT',
+    type: 'html',
+    tags: ['html', 'static', 'site', 'generator', 'markdown', 'processor', 'markdown-processor']
 )]
-#[Service()]
 #[Compatibility(framework: '>=0.1.0', php: '>=8.3')]
-#[Provides(interface: ForgeMarkDownInterface::class, version: '0.1.3')]
+#[Provides(interface: ForgeMarkDownInterface::class, version: '0.1.4')]
 class ForgeMarkDownModule
 {
-  public function register(Container $container): void
-  {
-    $container->bind(ForgeMarkDownInterface::class, ForgeMarkDown::class);
-  }
+    public function register(Container $container): void
+    {
+        $container->bind(ForgeMarkDownInterface::class, ForgeMarkDown::class);
+    }
 }
