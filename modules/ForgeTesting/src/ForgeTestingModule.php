3293d6476d1630e67fa9c6183a78f9f53792866a
Jeremias Nunez <jeremias2@gmail.com>
2026-06-30 16:44:52 -0400

    Bump ForgeTesting module version to v0.4.5

--- modules/ForgeTesting/src/ForgeTestingModule.php
+++ modules/ForgeTesting/src/ForgeTestingModule.php
@@ -10,7 +10,7 @@ use Forge\Core\Module\Attributes\Repository;
 
 #[Module(
     name: "ForgeTesting",
-    version: "0.4.4",
+    version: "0.4.5",
     description: "A Test Suite Module By Forge",
     order: 9999,
     isCli: true,
