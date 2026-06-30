af86152c48e23c543d7162af9b4126ac1503f8f7
Jeremias Nunez <jeremias2@gmail.com>
2026-06-30 08:16:41 -0400

    Bump ForgeLanguage module version to v0.2.4

--- modules/ForgeLanguage/src/ForgeLanguageModule.php
+++ modules/ForgeLanguage/src/ForgeLanguageModule.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\ForgeLanguage;
 
 use Forge\Core\DI\Container;
+use Forge\Core\Module\Attributes\Requires;
 use Modules\ForgeRouter\Http\Request;
 use Forge\Core\Module\Attributes\Compatibility;
 use Forge\Core\Module\Attributes\ConfigDefaults;
@@ -12,30 +13,10 @@ use Modules\ForgeRouter\Events\RouterHookAttribute;
 use Modules\ForgeRouter\Events\RouterHookName;
 use Forge\Core\Module\Attributes\Module;
 use Forge\Core\Module\Attributes\Repository;
-use Forge\Core\DI\Attributes\Service;
-use Forge\CLI\Traits\OutputHelper;
-use Forge\Core\Module\Attributes\Structure;
 
-#[Structure(structure: [
-    'controllers' => 'src/Controllers',
-    'services' => 'src/Services',
-    'migrations' => 'src/Database/Migrations',
-    'views' => 'src/UI/views',
-    'components' => 'src/UI/views/components',
-    'commands' => 'src/Commands',
-    'events' => 'src/Events',
-    'tests' => 'src/tests',
-    'models' => 'src/Models',
-    'dto' => 'src/Dto',
-    'seeders' => 'src/Database/Seeders',
-    'middlewares' => 'src/Middlewares',
-    'languages' => 'src/Languages'
-])]
-
-
-#[Service]
-#[Module(name: 'ForgeLanguage', version: '0.2.3', description: 'Multi language support to extend Forge Kernel', order: 40, author: 'Your Name', license: 'MIT', tags: [])]
+#[Module(name: 'ForgeLanguage', version: '0.2.4', description: 'Multi language support to extend Forge Kernel', order: 40, author: 'Your Name', license: 'MIT', tags: [])]
 #[Compatibility(framework: '>=0.1.0', php: '>=8.3')]
+#[Requires(module: "forge-router")]
 #[Repository(type: 'git', url: 'https://github.com/forge-kernel/kernel-module-registry')]
 #[ConfigDefaults(defaults: [
     "forge_language" => [
@@ -55,12 +36,6 @@ use Forge\Core\Module\Attributes\Structure;
 ])]
 final class ForgeLanguageModule
 {
-    use OutputHelper;
-    public function register(Container $container): void
-    {
-
-    }
-
     #[RouterHookAttribute(RouterHookName::BEFORE_REQUEST)]
     public function onBeforeRequest(Request $request): void
     {
