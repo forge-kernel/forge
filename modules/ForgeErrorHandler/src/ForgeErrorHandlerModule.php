abc63dee9354ec86ee73b4dfc2e42958b02d1eb9
Jeremias Nunez <jeremias2@gmail.com>
2026-07-06 00:09:07 -0400

    Bump ForgeErrorHandler module version to v1.2.8

--- modules/ForgeErrorHandler/src/ForgeErrorHandlerModule.php
+++ modules/ForgeErrorHandler/src/ForgeErrorHandlerModule.php
@@ -15,7 +15,7 @@ use Forge\CLI\Traits\OutputHelper;
 
 #[Module(
     name: 'ForgeErrorHandler',
-    version: '1.2.7',
+    version: '1.2.8',
     description: 'An error handler by Forge',
     order: 2,
     core: true,
