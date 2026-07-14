1f4c4ec7009b88dcf4162f80b8ac93d9f6a7106c
Jeremias Nunez <jeremias2@gmail.com>
2026-07-14 04:12:30 -0400

    Bump ForgeDeployment module version to v2.5.9

--- modules/ForgeDeployment/src/ForgeDeploymentModule.php
+++ modules/ForgeDeployment/src/ForgeDeploymentModule.php
@@ -10,11 +10,12 @@ use Forge\Core\Module\Attributes\Compatibility;
 use Forge\Core\Module\Attributes\ConfigDefaults;
 use Forge\Core\Module\Attributes\Module;
 use Forge\Core\Module\Attributes\Repository;
+use Forge\Core\Module\Traits\RegistersCommands;
 use Forge\CLI\Traits\OutputHelper;
 
 #[Module(
     name: 'ForgeDeployment',
-    version: '2.5.8',
+    version: '2.5.9',
     description: 'Deploy applications to cloud providers with automated provisioning',
     order: 99,
     author: 'Forge Team',
@@ -37,10 +38,32 @@ use Forge\CLI\Traits\OutputHelper;
 final class ForgeDeploymentModule
 {
     use OutputHelper;
+    use RegistersCommands;
 
     public function register(Container $container): void
     {
         $this->setupConfigDefaults($container);
+    }
+
+    protected function commands(): array
+    {
+        return [
+            \Modules\ForgeDeployment\Commands\ConfigureDnsCommand::class,
+            \Modules\ForgeDeployment\Commands\CreateServerCommand::class,
+            \Modules\ForgeDeployment\Commands\DeleteServerCommand::class,
+            \Modules\ForgeDeployment\Commands\DeployAppCommand::class,
+            \Modules\ForgeDeployment\Commands\DeployCommand::class,
+            \Modules\ForgeDeployment\Commands\DeployEnvCommand::class,
+            \Modules\ForgeDeployment\Commands\FixPermissionsCommand::class,
+            \Modules\ForgeDeployment\Commands\InitDeploymentConfigCommand::class,
+            \Modules\ForgeDeployment\Commands\ProvisionCommand::class,
+            \Modules\ForgeDeployment\Commands\ResetCommand::class,
+            \Modules\ForgeDeployment\Commands\ResumeCommand::class,
+            \Modules\ForgeDeployment\Commands\RollbackCommand::class,
+            \Modules\ForgeDeployment\Commands\SetupSslCommand::class,
+            \Modules\ForgeDeployment\Commands\StatusCommand::class,
+            \Modules\ForgeDeployment\Commands\UpdateCommand::class,
+        ];
     }
 
     private function setupConfigDefaults(Container $container): void
