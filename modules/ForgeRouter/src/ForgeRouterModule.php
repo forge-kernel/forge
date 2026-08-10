52c0cae0f7113550d1a7c9a553622180d7a49ffc
Jeremias Nunez <jeremias2@gmail.com>
2026-08-10 10:39:31 -0400

    Bump ForgeRouter module version to v1.0.38

--- modules/ForgeRouter/src/ForgeRouterModule.php
+++ modules/ForgeRouter/src/ForgeRouterModule.php
@@ -30,7 +30,7 @@ use Throwable;
 #[Module(name: "ForgeRouter",
     description: "Forge Router and Http",
     author: "Forge Team",
-    version: '1.0.37',
+    version: '1.0.38',
     type: "core",
     license: "MIT",
     tags: ["router", "http"],
